02c3992d76f57866171637fe1d7eec565501409b
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-05-19 12:53:05 +0700

    refactor: AppNew to use App

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,8 @@
-import { useEffect, useRef, useState } from "react";
-import { ec as EC } from "elliptic";
-import "./App.css";
-import { Socket, io } from "socket.io-client";
+import React from 'react';
+import AppRoutes from './routes';
 
-function App() {
-  const [message, setMessage] = useState("");
-  const socketRef = useRef<Socket | null>(null);
-  useEffect(() => {
-    const newSocket = io("http://localhost:8000");
-    socketRef.current = newSocket;
-    const ec = new EC("secp256k1");
-    const key = ec.genKeyPair();
-    const clientPublicKey = key.getPublic();
+const App: React.FC = () => {
+  return <AppRoutes />;
+};
 
-    newSocket.on('connect', () => {
-      console.log('Connected to server')
-
-      newSocket.emit('clientPublicKey', clientPublicKey.encode('hex', false))
-    })
-    newSocket.on('publicKey', (serverPublicKey) => {
-      console.log('Receive server public key')
-      const sharedSecret = key.derive(
-        ec.keyFromPublic(serverPublicKey, 'hex').getPublic(),
-      )
-      console.log('Shared secret:', sharedSecret)
-    })
-    // return () => newSocket.close();
-  }, []);
-  const hanldeSubmit = () => {
-    console.log("Submitted with message", message);
-    if (socketRef.current) {
-      socketRef.current.emit("sendMessage", message);
-    }
-  };
-  return (
-    <>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={hanldeSubmit}>Send</button>
-    </>
-  )
-}
-
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import AppNew from './AppNew.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppNew />
+    <App />
   </React.StrictMode>,
 )
