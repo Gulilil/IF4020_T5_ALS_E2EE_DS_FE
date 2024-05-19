558ca8f5a7bf5e967f0f2df172d67a92c5716e3f
Nathania Calista <nathania.calista01@gmail.com>
2024-05-19 16:10:56 +0700

    feat : save shared key to cookie

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { Login } from '@mui/icons-material';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/pages/Register';
+import TestingSocket from './components/pages/TestingSocket';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path='/socket' element={<TestingSocket />} />
       </Routes>
     </Router>
   );
