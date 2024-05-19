d828d652909b129a066cb16d0ee1a909855f1673
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-05-19 13:29:50 +0700

    feat: Update .env

--- src/api/endpoints/auth.ts
+++ src/api/endpoints/auth.ts
@@ -6,7 +6,8 @@ export const register = (userData: userData) => {
 };
 
 export const login = async (username: string, password: string) => {
-  const response = await apiClient.get(`/users?username=${username}&password=${password}`);
+  const response = await apiClient.post(`/auth/login`, {username, password});
+  console.log(response)
   if (response.data.length > 0) {
     return response.data[0];
   } else {
