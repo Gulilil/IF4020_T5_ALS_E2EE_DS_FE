77fb845145c975b6eb715114c8ebac3b2e77b0ef
Nathania Calista <nathania.calista01@gmail.com>
2024-05-21 21:53:24 +0700

    debug FE

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { encryptECB } from '../utils/ecb'
 import { adjustText, makeStringToBlocksArray } from '../utils/process'
+const SKIP_INTERCEPTOR_URLS = ['http://localhost:8000/api/']
 
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
@@ -9,23 +10,30 @@ const apiClient = axios.create({
   },
 })
 
+// Helper function to check if a URL should skip interceptors
+const shouldSkipInterceptor = (url) => {
+  return SKIP_INTERCEPTOR_URLS.some((skipUrl) => url === skipUrl)
+}
+
 apiClient.interceptors.request.use(
   (config) => {
     const cookieValue = document.cookie.replace(
       /(?:(?:^|.*;\s*)sharedKey\s*\=\s*([^;]*).*$)|^.*$/,
       '$1',
     )
-    if (config.data) {
-      console.log('Plain data ; ', config.data)
-      const stringData = JSON.stringify(config.data)
-      const textAdjusted = adjustText(stringData)
-      const textBlocks = makeStringToBlocksArray(textAdjusted, false)
-      const key = makeStringToBlocksArray(cookieValue, true)[0]
-      const encrypedData = encryptECB(textBlocks, key)
-      const data = encrypedData.join(',')
-      const encodedData = Buffer.from(data, 'base64').toString('utf8')
-      config.data = encodedData
-    }
+    console.log('Ini cookie', cookieValue)
+    const url = config.baseURL + config.url
+    console.log('Ini url ', url)
+    const stringData = JSON.stringify(config.data)
+    const textAdjusted = adjustText(stringData)
+    const textBlocks = makeStringToBlocksArray(textAdjusted, false)
+    const key = makeStringToBlocksArray(cookieValue, true)[0]
+    console.log('Ini key', key)
+    const encryptedData = encryptECB(textBlocks, key)
+    const data = encryptedData.join(',')
+    const encodedData = Buffer.from(data).toString('base64')
+    console.log('Ini encoded data', encodedData)
+    config.data = encodedData
     return config
   },
   (error) => {
